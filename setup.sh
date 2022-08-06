b7a89daff4c88d3473fa6824d6ab000bc1079b47
Saurabh Sharan <saurabh.sharan1@gmail.com>
2022-08-06 00:08:32 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,22 @@
 # See here for why https://tonsky.me/blog/monitors/
 defaults write-currentHost -g AppleFontSmoothing -int 0
 
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable auto-capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# Disable adding period after double space
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Enable dragging window from anywhere while holding Ctrl + Cmd
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
+# Don't display thumbnail after taking a screenshot
+defaults write com.apple.screencapture show-thumbnail -bool false
+
+# Use jpgs instead of pngs for screenshots since much more space-efficient
+defaults write com.apple.screencapture type -string jpg
+
 # Re-enable slow animations while holding down Shift
 defaults write com.apple.dock slow-motion-allowed -bool true
 
@@ -21,8 +37,14 @@ sudo defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -int 0
 
+# Hide recent apps from Dock
+defaults write com.apple.dock show-recents -bool false
+
 # Always show full file extension in Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Don't display warning when changing file extension in Finder
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Make crash reports appear as notifications
 defaults write com.apple.CrashReporter UseUNC 1
