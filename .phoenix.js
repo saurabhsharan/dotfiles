76ceb60987f4b47701deb4788b7550f412579424
Saurabh Sharan <saurabh.sharan1@gmail.com>
2023-06-02 17:05:26 -0700

    Update .phoenix.js with better Ctrl+Shift+J/K support for moving window focus

--- .phoenix.js
+++ .phoenix.js
@@ -1,85 +1,139 @@
+// .phoenix.js - this file is loaded automatically by phoenix
+
 const INCREMENT_WIDTH = 200;
 
 function isOverlap(window1, window2) {
   let f1 = window1.frame();
   let f2 = window2.frame();
 
-  return !(f1.x >= f2.x + f2.width ||
-          f2.x >= f1.x + f1.width ||
-          f1.y >= f2.y + f2.height ||
-          f2.y >= f1.y + f1.height);
+  return !(
+    f1.x >= f2.x + f2.width ||
+    f2.x >= f1.x + f1.width ||
+    f1.y >= f2.y + f2.height ||
+    f2.y >= f1.y + f1.height
+  );
 }
 
 // Finds three windows that are tiled side-by-side
 function findThreeTiledWindows() {
+  let currentWindow = Window.focused();
   let windows = Window.recent();
 
   let screenFrame = Space.active().screens()[0].visibleFrame();
 
   // loop over all possible 3 window combinations
   for (let i = 0; i < windows.length; i++) {
-    for (let j = i+1; j < windows.length; j++) {
-      for (let k = j+1; k < windows.length; k++) {
-        let firstWindow = windows[i];
-        let secondWindow = windows[j];
-        let thirdWindow = windows[k];
+    for (let j = i + 1; j < windows.length; j++) {
+      let firstWindow = currentWindow;
+      let secondWindow = windows[i];
+      let thirdWindow = windows[j];
 
-        if (isOverlap(firstWindow, secondWindow) || isOverlap(firstWindow, thirdWindow) || isOverlap(secondWindow, thirdWindow)) {
-          continue;
-        }
+      if (
+        isOverlap(firstWindow, secondWindow) ||
+        isOverlap(firstWindow, thirdWindow) ||
+        isOverlap(secondWindow, thirdWindow)
+      ) {
+        continue;
+      }
 
-        let leftmostWindow = null;
-        let middleWindow = null;
-        let rightmostWindow = null;
+      let leftmostWindow = null;
+      let middleWindow = null;
+      let rightmostWindow = null;
 
-        if (firstWindow.frame().x < secondWindow.frame().x) {
-          if (firstWindow.frame().x < thirdWindow.frame().x) {
-            leftmostWindow = firstWindow;
-            if (secondWindow.frame().x < thirdWindow.frame().x) {
-              middleWindow = secondWindow;
-              rightmostWindow = thirdWindow;
-            } else {
-              middleWindow = thirdWindow;
-              rightmostWindow = secondWindow;
-            }
+      if (firstWindow.frame().x < secondWindow.frame().x) {
+        if (firstWindow.frame().x < thirdWindow.frame().x) {
+          leftmostWindow = firstWindow;
+          if (secondWindow.frame().x < thirdWindow.frame().x) {
+            middleWindow = secondWindow;
+            rightmostWindow = thirdWindow;
           } else {
-            leftmostWindow = thirdWindow;
-            middleWindow = firstWindow;
+            middleWindow = thirdWindow;
             rightmostWindow = secondWindow;
           }
         } else {
-          if (secondWindow.frame().x < thirdWindow.frame().x) {
-            leftmostWindow = secondWindow;
-            if (firstWindow.frame().x < thirdWindow.frame().x) {
-              middleWindow = firstWindow;
-              rightmostWindow = thirdWindow;
-            } else {
-              middleWindow = thirdWindow;
-              rightmostWindow = firstWindow;
-            }
+          leftmostWindow = thirdWindow;
+          middleWindow = firstWindow;
+          rightmostWindow = secondWindow;
+        }
+      } else {
+        if (secondWindow.frame().x < thirdWindow.frame().x) {
+          leftmostWindow = secondWindow;
+          if (firstWindow.frame().x < thirdWindow.frame().x) {
+            middleWindow = firstWindow;
+            rightmostWindow = thirdWindow;
           } else {
-            leftmostWindow = thirdWindow;
-            middleWindow = secondWindow;
+            middleWindow = thirdWindow;
             rightmostWindow = firstWindow;
           }
+        } else {
+          leftmostWindow = thirdWindow;
+          middleWindow = secondWindow;
+          rightmostWindow = firstWindow;
         }
-
-        // console.log(`the three window titles are: ${JSON.stringify([firstWindow.title(), secondWindow.title(), thirdWindow.title()])}`);
-        // console.log(`the three window frames are: ${JSON.stringify([firstWindow.frame(), secondWindow.frame(), thirdWindow.frame()])}`);
-
-        let firstWindowSize = firstWindow.frame().width * firstWindow.frame().height;
-        let secondWindowSize = secondWindow.frame().width * secondWindow.frame().height;
-        let thirdWindowSize = thirdWindow.frame().width * thirdWindow.frame().height;
-
-        if (firstWindowSize + secondWindowSize + thirdWindowSize == screenFrame.width * screenFrame.height) {
-          return [firstWindow, secondWindow, thirdWindow].sort((a, b) => a.frame().x - b.frame().x);
-        }
-
-        // if (firstWindow.frame().x + firstWindow.frame().width === secondWindow.frame().x &&
-        //     secondWindow.frame().x + secondWindow.frame().width === thirdWindow.frame().x) {
-        //   return [firstWindow, secondWindow, thirdWindow];
-        // }
       }
+
+      // console.log(`the three window titles are: ${JSON.stringify([firstWindow.title(), secondWindow.title(), thirdWindow.title()])}`);
+      // console.log(`the three window frames are: ${JSON.stringify([firstWindow.frame(), secondWindow.frame(), thirdWindow.frame()])}`);
+
+      let firstWindowSize =
+        firstWindow.frame().width * firstWindow.frame().height;
+      let secondWindowSize =
+        secondWindow.frame().width * secondWindow.frame().height;
+      let thirdWindowSize =
+        thirdWindow.frame().width * thirdWindow.frame().height;
+
+      if (
+        firstWindowSize + secondWindowSize + thirdWindowSize ==
+        screenFrame.width * screenFrame.height
+      ) {
+        let result = [firstWindow, secondWindow, thirdWindow].sort(
+          (a, b) => a.frame().x - b.frame().x
+        );
+        console.log(
+          `the three window titles are: ${JSON.stringify([
+            result[0].title(),
+            result[1].title(),
+            result[2].title(),
+          ])}`
+        );
+        return result;
+      }
+
+      // if (firstWindow.frame().x + firstWindow.frame().width === secondWindow.frame().x &&
+      //     secondWindow.frame().x + secondWindow.frame().width === thirdWindow.frame().x) {
+      //   return [firstWindow, secondWindow, thirdWindow];
+      // }
+    }
+  }
+
+  return null;
+}
+
+function findTwoTiledWindows() {
+  let currentWindow = Window.focused();
+  let windows = Window.recent();
+
+  let screenFrame = Space.active().screens()[0].visibleFrame();
+
+  for (const w2 of windows) {
+    let w1 = currentWindow;
+
+    if (w1.isEqual(w2) || isOverlap(w1, w2)) {
+      continue;
+    }
+
+    // console.log(`comparing ${w1.app().name()} (${w1.title()}) and ${w2.app().name()} (${w2.title()})`);
+
+    let w1Frame = w1.frame();
+    let w2Frame = w2.frame();
+
+    let w1Size = w1Frame.width * w1Frame.height;
+    let w2Size = w2Frame.width * w2Frame.height;
+
+    if (w1Size + w2Size === screenFrame.width * screenFrame.height) {
+      let result = [w1, w2].sort((a, b) => a.frame().x - b.frame().x);
+      console.log(`the two window titles are: ${JSON.stringify([result[0].title(), result[1].title()])}`);
+      return result;
     }
   }
 
@@ -92,16 +146,27 @@ function retileTwoWindows(direction) {
   let screen = currentSpace.screens()[0];
   let screenFrame = screen.visibleFrame();
 
-  let windows = currentSpace.windows({}).filter(wndow => wndow.isVisible() === true);
+  // console.log(`screen frame is ${JSON.stringify(screenFrame)}`);
+
+  var start = Date.now();
+  let recentWindows = Window.recent();
+  console.log(`took ${Date.now() - start} ms to call Window.recent()`);
+  let windows = recentWindows.filter((wndow) => wndow.isVisible() === true);
+
+  // console.log(`checking windows in order: ${windows.map(window => window.app().name())}`);
 
   // find two windows whose size adds up to screen
+  start = Date.now();
   let window1 = null;
   let window2 = null;
+  let breakCheck1 = false; // break out of outer loop
   for (const w1 of windows) {
     for (const w2 of windows) {
-      if (w1 === w2) {
+      if (w1.isEqual(w2) || isOverlap(w1, w2)) {
         continue;
       }
+
+      // console.log(`comparing ${w1.app().name()} (${w1.title()}) and ${w2.app().name()} (${w2.title()})`);
 
       let w1Frame = w1.frame();
       let w2Frame = w2.frame();
@@ -112,17 +177,28 @@ function retileTwoWindows(direction) {
       if (w1Size + w2Size === screenFrame.width * screenFrame.height) {
         window1 = w1;
         window2 = w2;
+
+        breakCheck1 = true;
         break;
       }
+    }
+
+    if (breakCheck1) {
+      break;
     }
   }
 
   if (window1 === null || window2 === null) {
-    console.log('could not find windows');
+    console.log("could not find windows");
     return;
   }
 
-  console.log(`found windows ${window1.title()} and ${window2.title()}`);
+  console.log(`took ${Date.now() - start} ms to find windows`);
+  console.log(
+    `found windows ${window1.app().name()} (${window1.title()}) and  ${window2
+      .app()
+      .name()} (${window2.title()})`
+  );
 
   let leftWindow = null;
   let rightWindow = null;
@@ -145,82 +221,126 @@ function retileTwoWindows(direction) {
   rightFrame.width += INCREMENT_WIDTH * direction;
   rightFrame.x -= INCREMENT_WIDTH * direction;
 
+  start = Date.now();
   leftWindow.setFrame(leftFrame);
   rightWindow.setFrame(rightFrame);
+  console.log(`took ${Date.now() - start} ms to set frames`);
+}
+
+// Detects whether the current window config is either two-window tile, three-window tile, or not a tile
+function getTiledWindowsConfig() {
+  // First check for three-window tile, since that config is less likely to occur by accident
+  let threeTiledWindows = findThreeTiledWindows();
+  if (threeTiledWindows !== null) {
+    return { type: "three-tile", windows: threeTiledWindows };
+  }
+
+  let twoTiledWindows = findTwoTiledWindows();
+  if (twoTiledWindows !== null) {
+    return { type: "two-tile", windows: twoTiledWindows };
+  }
+
+  return { type: "none", windows: [] };
 }
 
 // When two windows are tiled side-by-side, Ctrl-Shift-H will adjust the split to increase the width of the *right* window
-Key.on('h', ['ctrl', 'shift'], () => {
+Key.on("h", ["ctrl", "shift"], () => {
+  console.log(`got ctrl-shift-h`);
   // const allSpaces = Space.all();
 
   // for (var i = 0; i < allSpaces.length; ++i) {
-    // let space = allSpaces[i];
-    // let spaceWindows = space.windows({});
+  // let space = allSpaces[i];
+  // let spaceWindows = space.windows({});
 
-    // for (const wndow of spaceWindows) {
-      // let app = wndow.app();
-      // console.log(`space ${i} app ${app.name()} window ${wndow.title()}`);
-    // }
+  // for (const wndow of spaceWindows) {
+  // let app = wndow.app();
+  // console.log(`space ${i} app ${app.name()} window ${wndow.title()}`);
+  // }
   // }
 
+  var start = Date.now();
   retileTwoWindows(1);
+  console.log(`took ${Date.now() - start} ms to retile`);
 });
 
 // When two windows are tiled side-by-side, Ctrl-Shift-L will adjust the split to increase the width of the *left* window
-Key.on('l', ['ctrl', 'shift'], () => {
+Key.on("l", ["ctrl", "shift"], () => {
+  console.log(`got ctrl-shift-l`);
+  var start = Date.now();
   retileTwoWindows(-1);
+  console.log(`took ${Date.now() - start} ms to retile`);
 });
 
-// When three windows are tiled side-by-side, Ctrl-Shift-K will focus the window to the right of the currently focused window
-Key.on('k', ['ctrl', 'shift'], () => {
-  let threeWindows = findThreeTiledWindows();
-  if (threeWindows === null) {
-    console.log('could not find three windows');
+// When windows are tiled (either two-way or three-way), Ctrl+Shift+K will move window focus to the spatially east window.
+Key.on("k", ["ctrl", "shift"], () => {
+  let currentWindow = Window.focused();
+  let tiledWindows = getTiledWindowsConfig();
+  // console.log(`tiled windows config: ${JSON.stringify(tiledWindows)}`);
+  // console.log(`windows: ${JSON.stringify(tiledWindows.windows.map((wndow) => wndow.title()))}`);
+
+  if (tiledWindows.type === "none") {
     return;
   }
 
-  console.log(`three window titles: ${JSON.stringify(threeWindows.map(wndow => wndow.title()))}`);
+  if (tiledWindows.type === "three-tile") {
+    let leftWindow = tiledWindows.windows[0];
+    let middleWindow = tiledWindows.windows[1];
+    let rightWindow = tiledWindows.windows[2];
 
-  let focusedWindowHash = Window.focused().hash();
+    if (currentWindow.hash() === leftWindow.hash()) {
+      middleWindow.focus();
+    } else if (currentWindow.hash() === middleWindow.hash()) {
+      rightWindow.focus();
+    } else if (currentWindow.hash() === rightWindow.hash()) {
+      leftWindow.focus();
+    }
+  }
 
-  let leftWindow = threeWindows[0];
-  let middleWindow = threeWindows[1];
-  let rightWindow = threeWindows[2];
+  if (tiledWindows.type === "two-tile") {
+    let leftWindow = tiledWindows.windows[0];
+    let rightWindow = tiledWindows.windows[1];
 
-  if (leftWindow.hash() === focusedWindowHash) {
-    middleWindow.focus();
-  } else if (middleWindow.hash() === focusedWindowHash) {
-    rightWindow.focus();
-  } else if (rightWindow.hash() === focusedWindowHash) {
-    leftWindow.focus();
-  } else {
-    console.log(`didn't focus any window`);
+    if (currentWindow.hash() === leftWindow.hash()) {
+      rightWindow.focus();
+    } else if (currentWindow.hash() === rightWindow.hash()) {
+      leftWindow.focus();
+    }
   }
 });
 
-// When three windows are tiled side-by-side, Ctrl-Shift-J will focus the window to the left of the currently focused window
-Key.on('j', ['ctrl', 'shift'], () => {
-  let threeWindows = findThreeTiledWindows();
-  if (threeWindows === null) {
-    console.log('could not find three windows');
+// When windows are tiled (either two-way or three-way), Ctrl+Shift+J will move window focus to the spatially west window.
+Key.on("j", ["ctrl", "shift"], () => {
+  let currentWindow = Window.focused();
+  let tiledWindows = getTiledWindowsConfig();
+  // console.log(`tiled windows config: ${JSON.stringify(tiledWindows)}`);
+  // console.log(`windows: ${JSON.stringify(tiledWindows.windows.map((wndow) => wndow.title()))}`);
+
+  if (tiledWindows.type === "none") {
     return;
   }
 
-  console.log(`three window titles: ${JSON.stringify(threeWindows.map(wndow => wndow.title()))}`);
+  if (tiledWindows.type === "three-tile") {
+    let leftWindow = tiledWindows.windows[0];
+    let middleWindow = tiledWindows.windows[1];
+    let rightWindow = tiledWindows.windows[2];
 
-  let focusedWindowHash = Window.focused().hash();
+    if (currentWindow.hash() === leftWindow.hash()) {
+      rightWindow.focus();
+    } else if (currentWindow.hash() === middleWindow.hash()) {
+      leftWindow.focus();
+    } else if (currentWindow.hash() === rightWindow.hash()) {
+      middleWindow.focus();
+    }
+  }
 
-  let leftWindow = threeWindows[0];
-  let middleWindow = threeWindows[1];
-  let rightWindow = threeWindows[2];
+  if (tiledWindows.type === "two-tile") {
+    let leftWindow = tiledWindows.windows[0];
+    let rightWindow = tiledWindows.windows[1];
 
-  if (leftWindow.hash() === focusedWindowHash) {
-    rightWindow.focus();
-  } else if (middleWindow.hash() === focusedWindowHash) {
-    leftWindow.focus();
-  } else if (rightWindow.hash() === focusedWindowHash) {
-    middleWindow.focus();
-  } else {
-    console.log(`didn't focus any window`);
+    if (currentWindow.hash() === leftWindow.hash()) {
+      rightWindow.focus();
+    } else if (currentWindow.hash() === rightWindow.hash()) {
+      leftWindow.focus();
+    }
   }
 });
