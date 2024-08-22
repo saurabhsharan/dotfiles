3cba7e00e3d204359ddde3313fb6ef54d1506382
Saurabh Sharan <saurabh.sharan1@gmail.com>
2024-08-22 16:36:54 -0700

    Update WezTerm config

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,15 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.default_prog = { "fish" }
+
 config.color_scheme = "GitHub Dark"
+
+config.enable_scroll_bar = true
+
+config.font = wezterm.font("Source Code Pro", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font_size = 13
+
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
 
 return config
