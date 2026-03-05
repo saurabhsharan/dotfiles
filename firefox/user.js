bb8a331573a8fb6948dd2cb1591bc3da38b9168a
Saurabh Sharan <806467+saurabhsharan@users.noreply.github.com>
2026-03-04 23:20:17 -0800

    Add Firefox user.js

---
+++ firefox/user.js
@@ -0,0 +1,29 @@
+user_pref("browser.startup.page", 3);
+user_pref("general.autoScroll", false);
+
+user_pref("browser.tabs.unloadOnLowMemory", false);
+
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("security.dialog_enable_delay", 0);
+
+user_pref("network.http.sendRefererHeader", 0);
+
+// Helps prevent phishing attacks
+user_pref("network.IDN_show_punycode", true);
+
+// Disable various Mozilla commerce/upsell-related features
+user_pref("extensions.pocket.enabled", false);
+user_pref("toolkit.shopping.environment", "");
+user_pref("browser.shopping.experience2023.active", false);
+user_pref("browser.shopping.experience2023.ads.enabled", false);
+user_pref("browser.shopping.experience2023.ads.userEnabled", false);
+user_pref("browser.shopping.experience2023.autoOpen.enabled", false);
+user_pref("browser.shopping.experience2023.autoOpen.userEnabled", false);
+user_pref("browser.shopping.experience2023.enabled", false);
+user_pref("browser.shopping.experience2023.optedIn", 0);
+user_pref("browser.shopping.experience2023.survey.enabled", false);
+user_pref("browser.shopping.experience2023.shoppingSidebar", false);
+user_pref("browser.shopping.experience2023.integratedSidebar", false);
+user_pref("browser.shopping.experience2023.autoClose.userEnabled", false);
+user_pref("toolkit.shopping.experience2023.defr", false);
+
