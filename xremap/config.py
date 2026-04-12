f640defcd96e804da4bd94918e1d1842d4c2df4e
Saurabh Sharan <806467+saurabhsharan@users.noreply.github.com>
2026-04-11 21:54:20 -0700

    Update xremap/config.py

--- xremap/config.py
+++ xremap/config.py
@@ -20,67 +20,7 @@ APPLE_KEYBOARDS = [
 ]
 
 TERMINALS = ["org.wezfurlong.wezterm", "kitty", "com.mitchellh.ghostty"]
-EDITORS = ["dev.zed.Zed", "jetbrains-idea"]
 CHROME_BROWSERS = ["chromium", "google-chrome"]
-ALL_BROWSERS = ["firefox", "Vivaldi-stable"] + CHROME_BROWSERS
-
-# ==========================================
-# SNIPPETS (Shared Keybindings)
-# ==========================================
-MAC_TEXT_NAVIGATION = {
-    "Alt-left": "Ctrl-left",
-    "Alt-right": "Ctrl-right",
-    "Alt-up": "Ctrl-home",
-    "Alt-down": "Ctrl-end",
-
-    "Alt-Shift-left": "Ctrl-Shift-left",
-    "Alt-Shift-right": "Ctrl-Shift-right",
-    "Alt-Shift-up": "Ctrl-Shift-home",
-    "Alt-Shift-down": "Ctrl-Shift-end",
-
-    "Super-left": "home",
-    "Super-right": "end",
-
-    "Super-Shift-left": "Shift-home",
-    "Super-Shift-right": "Shift-end",
-
-    "Alt-backspace": "Ctrl-backspace",
-    "Super-backspace": ["Shift-home", "delete"],
-
-    "Super-a": "Ctrl-a", # select all
-}
-
-EMACS_NAV = {
-    "Ctrl-a": "home",
-    "Ctrl-b": "left",
-    "Ctrl-d": "delete",
-    "Ctrl-e": "end",
-    "Ctrl-f": "right",
-    "Ctrl-h": "backspace",
-    "Ctrl-k": ["Shift-end", "delete"],
-    "Ctrl-Alt-b": "Ctrl-left",
-    "Ctrl-Alt-f": "Ctrl-right",
-
-    # These are custom and not Emacs-standard
-    "Alt-f": "Ctrl-right",
-    "Alt-b": "Ctrl-left",
-    "Alt-Shift-b": "Ctrl-Shift-left",
-    "Alt-Shift-f": "Ctrl-Shift-right",
-}
-
-BROWSER_BASE = {
-    "Super-l": "Ctrl-l",
-    "Super-t": "Ctrl-t",
-    "Super-w": "Ctrl-w",
-    "Super-minus": "Ctrl-minus",
-    "Super-equal": "Ctrl-equal",
-    "Alt_R-minus": "Ctrl-minus",
-    "Alt_R-equal": "Ctrl-equal",
-    "Super-leftbrace": "Ctrl-leftbrace",
-    "Super-rightbrace": "Ctrl-rightbrace",
-    "Alt_R-Shift-rightbrace": "Ctrl-tab",
-    "Alt_R-Shift-leftbrace": "Ctrl-Shift-tab",
-}
 
 # ==========================================
 # MODMAPS (Hardware Key Reassignments)
@@ -116,139 +56,141 @@ class AssignFnToLeftCtrl(Modmap):
     device = Device(only='Apple Inc. Magic Keyboard')
 
 # ==========================================
+# SNIPPETS (Shared Keybindings)
+# ==========================================
+MAC_TEXT_NAVIGATION = {
+    "Alt-left": "Ctrl-left",
+    "Alt-right": "Ctrl-right",
+    "Alt-up": "Ctrl-home",
+    "Alt-down": "Ctrl-end",
+
+    "Alt-Shift-left": "Ctrl-Shift-left",
+    "Alt-Shift-right": "Ctrl-Shift-right",
+    "Alt-Shift-up": "Ctrl-Shift-home",
+    "Alt-Shift-down": "Ctrl-Shift-end",
+
+    "Super-left": "home",
+    "Super-right": "end",
+
+    "Super-Shift-left": "Shift-home",
+    "Super-Shift-right": "Shift-end",
+
+    "Alt-backspace": "Ctrl-backspace",
+    "Super-backspace": ["Shift-home", "delete"],
+
+    "Super-a": "Ctrl-a", # Select All
+}
+
+EMACS_NAV = {
+    "Ctrl-a": "home",
+    "Ctrl-b": "left",
+    "Ctrl-d": "delete",
+    "Ctrl-e": "end",
+    "Ctrl-f": "right",
+    "Ctrl-h": "backspace",
+    "Ctrl-k": ["Shift-end", "delete"],
+    "Ctrl-Alt-b": "Ctrl-left",
+    "Ctrl-Alt-f": "Ctrl-right",
+
+    # These are custom and not Emacs-standard
+    "Alt-f": "Ctrl-right",
+    "Alt-b": "Ctrl-left",
+    "Alt-Shift-b": "Ctrl-Shift-left",
+    "Alt-Shift-f": "Ctrl-Shift-right",
+}
+
+# ==========================================
 # KEYMAPS (Application Keybindings)
 # ==========================================
-class ArrowKeys(Keymap):
-    name = "Arrow keys"
+class GlobalBindings(Keymap):
+    name = "Global bindings"
     exact_match = True
-    remap = MAC_ARROWS | {
+    remap = MAC_TEXT_NAVIGATION | {
         "Ctrl-i": "up",
         "Ctrl-j": "down",
+    } | {
+        "Super-c": "Ctrl-c", # copy
+        "Super-v": "Ctrl-v", # paste
+        "Super-x": "Ctrl-x", # cut
     }
+
+class EmacsBindings(Keymap):
+    name = "Emacs bindings"
+    exact_match = True
+    # Exclude terminal emulators from Emacs bindings since they are often already built-in and/or conflict with the shell/editor
+    application = App(not_=TERMINALS)
+    remap = EMACS_NAV
 
 class BaseBrowser(Keymap):
     is_base = True
     exact_match = True
-    remap = BROWSER_BASE | {
-        f"Super-{i}": f"Ctrl-{i}" for i in range(1, 10)
-    } | {
-        "Super-0": "Ctrl-0",
+    remap = {
+        "Super-d": "Ctrl-d", # bookmark current page
+        "Super-f": "Ctrl-f", # find
+        "Super-g": "Ctrl-g", # jump to next match
+        "Super-Shift-g": "Ctrl-Shift-g", # jump to previous match
+        "Super-l": "Ctrl-l", # focus address bar
+        "Super-n": "Ctrl-n", # new window
+        "Super-r": "Ctrl-r", # refresh
+        "Super-t": "Ctrl-t", # new tab
+        "Super-w": "Ctrl-f4", # close tab
+        "Super-0": "Ctrl-0", # reset zoom
+        "Super-leftbrace": "Alt-left", # go back
+        "Super-rightbrace": "Alt-right", # go forward
+        "Super-Shift-o": "Ctrl-Shift-o", # open bookmarks
+        "Super-Shift-t": "Ctrl-Shift-t", # reopen closed tab
+        "Super-Alt-i": "Ctrl-Shift-i", # open inspector
+        "Super-minus": "Ctrl-minus", # zoom out
+        "Super-equal": "Ctrl-equal", # zoom in
+        "Super-leftbrace": "Ctrl-leftbrace", # previous tab
+        "Super-rightbrace": "Ctrl-rightbrace", # next tab
+
+        "Alt_R-leftbrace": "Alt-left", # go back
+        "Alt_R-rightbrace": "Alt-right", # go forward
+        "Alt_R-minus": "Ctrl-minus", # zoom out
+        "Alt_R-equal": "Ctrl-equal", # zoom in
+        "Alt_R-Shift-rightbrace": "Ctrl-tab", # next tab
+        "Alt_R-Shift-leftbrace": "Ctrl-Shift-tab", # previous tab
     }
 
 class Firefox(BaseBrowser):
     name = "Firefox"
+    exact_match = True
     application = App(only=["firefox"])
     remap = BaseBrowser.remap | {
-        "Super-n": "Ctrl-n",
-        **{f"Super-{i}": f"Alt-{i}" for i in range(1, 10)},
-        "Super-0": "Alt-0",
+        "Super-Shift-p": "Ctrl-Shift-p", # new private window
+    } | {
+        **{f"Super-{i}": f"Alt-{i}" for i in range(1, 10)}, # select tab by position
     }
 
 class Chrome(BaseBrowser):
     name = "Chrome"
-    application = App(only=CHROME_BROWSERS)
-    remap = BaseBrowser.remap | EMACS_NAV | {
-        "Super-a": "Ctrl-a",
-        "Super-d": "Ctrl-d",
-        "Super-f": "Ctrl-f",
-        "Super-g": "Ctrl-g",
-        "Super-n": "Ctrl-n",
-        "Super-r": "Ctrl-r",
-        "Super-w": "Ctrl-f4",
-        "Super-comma": "Ctrl-Shift-comma",
-        "Super-Shift-a": "Ctrl-Shift-a",
-        "Super-Shift-c": "Ctrl-Shift-c",
-        "Super-Shift-n": "Ctrl-Shift-n",
-        "Super-Shift-o": "Ctrl-Shift-o",
-        "Super-Shift-t": "Ctrl-Shift-t",
-        "Super-Alt-i": "Ctrl-Shift-i",
-        "Super-leftbrace": "Alt-left",
-        "Super-rightbrace": "Alt-right",
-        "Alt_R-leftbrace": "Alt-left",
-        "Alt_R-rightbrace": "Alt-right",
-        "Ctrl-f": "right", 
-    }
-    remap.pop("Super-0", None)
-
-class RoamAUR(Keymap):
-    name = "Roam Research (AUR version)"
     exact_match = True
-    application = App(only=["Roam Research"])
-    remap = {
-        "Ctrl-f": "right",
-        "Super-u": "Ctrl-u",
+    application = App(only=CHROME_BROWSERS)
+    remap = BaseBrowser.remap | {
+        "Super-comma": "Ctrl-Shift-comma", # open settings
+
+        "Super-Shift-a": "Ctrl-Shift-a", # search tabs
+        "Super-Shift-c": "Ctrl-Shift-c", # select element to inspect
+        "Super-Shift-n": "Ctrl-Shift-n", # new incognito window
+
+        "Alt-Super-b": "Ctrl-Shift-o", # open bookmarks
+    } | {
+        f"Super-{i}": f"Ctrl-{i}" for i in range(1, 10) # select tab by position
     }
 
 class RoamPWA(Keymap):
     name = "Roam  Research (Chrome PWA)"
     exact_match = True
     application = App(only=["chrome-lflgehidkjooeaeclhaadoefaleoeged-Default"])
-    remap = EMACS_NAV | {
+    remap = {
         "Ctrl-n": "down",
-        "Ctrl-u": ["Shift-home", "delete"],
-        "Super-a": "Ctrl-a",
-        "Ctrl-f": "right",
     }
 
-class RoamViewerPWA(Keymap):
+class RoamViewerPWA(Chrome):
     name = "Roam Viewer (Chrome PWA)"
     exact_match = True
     application = App(only=["chrome-jcgpenhgafmhijpfmgmlcacapeaofcdf-Default", "chrome-nmhgeifmehldnegmaackjamblpjkcpmh-Default"])
-    remap = EMACS_NAV | {
-        "Ctrl-Alt-s": "pagedown",
-        "Ctrl-Alt-w": "pageup",
-        "Super-a": "Ctrl-a",
-        "Super-f": "Ctrl-f",
-        "Super-g": "Ctrl-g",
-        "Super-r": "Ctrl-r",
-        "Super-Shift-g": "Ctrl-Shift-g",
-        "Super-minus": "Ctrl-minus",
-        "Super-equal": "Ctrl-equal",
-        "Alt_R-minus": "Ctrl-minus",
-        "Alt_R-equal": "Ctrl-equal",
-        "Ctrl-k": ["Shift-end", "delete"],
-    }
-
-class ZedEditor(Keymap):
-    name = "Zed Editor"
-    exact_match = True
-    application = App(only=["dev.zed.Zed"])
-    remap = {
-        "Ctrl-a": "home",
-        "Ctrl-b": "left",
-        "Ctrl-e": "end",
-        "Ctrl-f": "right",
-        "Ctrl-n": "down",
-        "Ctrl-p": "up",
-        "Super-c": "Ctrl-c",
-        "Super-p": "Ctrl-p",
-        "Super-s": "Ctrl-s",
-        "Super-v": "Ctrl-v",
-        "Super-w": "Ctrl-w",
-        "Super-x": "Ctrl-x",
-        "Super-z": "Ctrl-z",
-        "Super-Shift-w": "Ctrl-Shift-w",
-        "Super-Shift-p": "Ctrl-Shift-p",
-    }
-
-class NonWebAndNonTerm(Keymap):
-    name = "Non-web browser and non-terminal"
-    exact_match = True
-    application = App(not_=ALL_BROWSERS + TERMINALS + EDITORS)
-    remap = {
-        "Super-q": "Ctrl-q",
-        "Super-w": "Alt-f4",
-    }
-
-class NonTerminal(Keymap):
-    name = "Non-terminal"
-    exact_match = True
-    application = App(not_=["org.wezfurlong.wezterm", "dev.zed.Zed"])
-    remap = {
-        "Super-c": "Ctrl-c",
-        "Super-v": "Ctrl-Shift-v",
-        "Super-x": "Ctrl-x",
-    }
 
 if __name__ == "__main__":
     compile_config("config.yml")
